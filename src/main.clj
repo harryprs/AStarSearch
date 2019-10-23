c8c8569ef4b9c62e917a7d6bc0fd896f6a3333d1
HARRY PARSONS <S6118618@live.tees.ac.uk>
2019-10-23 14:12:19 +0100

    Added modify-cost, get-cost, made map an atom

--- src/main.clj
+++ src/main.clj
@@ -6,17 +6,19 @@
                           get-cost :cost
                           selector :undef
                           debug    false}}]
-  (let [goal? (if (fn? goal)
-                #(when (goal %) %)
-                #(when (= % goal) %))
-        member? (fn [lis x] (some (partial = x) lis))
+  (let [member? (fn [lis x] (some (partial = x) lis))
         selector (if-not (= selector :undef)  ;it was a key arg
                    selector                  ; se leave it as is, else set it as default
                    (fn [bag]  (first (sort-by (comp get-cost first) bag))))]
 
+
     (loop [queued  `( (~start) )
            visited nil
+           goal goal                                        ;NEW CODE: GOAL PARAM
            ]
+      (let [goal? (if (fn? goal)                            ;NEW CODE: anonymous function, goal param to find
+                    #(when (goal %) %)
+                    #(when (= % goal) %))]
       (if (empty? queued) nil                      ;; fail if (null queued)
                           (let [next      (selector queued)          ;; select next node
                                 state     (first next)               ;; filter out path
@@ -32,7 +34,9 @@
 
                               :else
                               (if (member? visited raw-state)
-                                (recur (remove #(= % next) queued) visited)
+                                (recur (remove #(= % next) queued)
+                                       visited
+                                       goal)
                                 (let [queued      (remove #(= % next) queued)
                                       moves       (LMG state)
                                       new-visited (cons raw-state visited)
@@ -48,22 +52,67 @@
 
                                   (recur
                                     (concat queued new-states)
-                                    new-visited)
+                                    new-visited
+                                    (fn [x] (= x "H")))     ;NEW CODE: NEW GOAL PARAM
                                   ))
                               ))
-                          ))))
+                          )))))
 
-(def graph
-  {"A" #{"B"}
-   "B" #{"A", "C", "D"}
-   "C" #{"B", "G"}
-   "D" #{"B", "E"}
-   "E" #{"D", "F"}
-   "F" #{"E", "I"}
-   "G" #{"C", "H"}
-   "H" #{"G", "I"}
-   "I" #{"H", "F"}
-   })
+
+
+
+
+(defn newgoal []
+
+  )
+
+
+;  "Map of node => adjacent node => cost. This could
+;   be replaced with any cost function of the shape
+;   (node, node') => cost."
+(def graph (atom {"A" {"B" 2, "E" 10}
+                      "B" {"A" 2, "C" 3, "D" 4}
+                      "C" {"B" 3, "D" 2}
+                      "D" {"B" 4, "C" 3, "E" 10}
+                      "E" {"A" 10, "D" 10}}))
+
+
+
+;get the cost to move from node x to y
+(defn get-cost [x, y]
+  (if (contains? (@graph x) y)
+    (get (@graph x) y)
+    (print (":("))
+    )
+  )
+
+
+;node x, node y,
+(defn modify-cost [x, y, cval]
+  ; update arc cost with cval
+  ;(get (swap! graph (assoc-in graph [x y] cval)) y)
+  (swap! graph #(assoc-in % [x y] cval))
+  (get-cost x y)
+  ; if
+  )
+
+
+(defn update-arc-cost [x, y]
+  )
+
+
+;
+;(def graph
+;  {"A" #{"B"}
+;   "B" #{"A", "C", "D"}
+;   "C" #{"B", "G"}
+;   "D" #{"B", "E"}
+;   "E" #{"D", "F"}
+;   "F" #{"E", "I"}
+;   "G" #{"C", "H"}
+;   "H" #{"G", "I"}
+;   "I" #{"H", "F"}
+;   })
 
 
 (defn movenode [node]                                       ;node = node to be moved
@@ -103,7 +152,7 @@
             (println node)
             (if (= i m)                                   ;update y as node
               (println m)
-              (println i)
+              ;(println i)
               )
             )
           )
